316ab3b6605bd0e70b4b41752ef00a53cf9c23b7
czh <2766694258@qq.com>
2023-08-14 17:09:06 +0800

    初始化

---
+++ czh-admin/src/main/java/com/wp/czhadmin/CzhAdminApplication.java
@@ -0,0 +1,15 @@
+package com.wp.czhadmin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CzhAdminApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(CzhAdminApplication.class, args);
+    }
+
+}
